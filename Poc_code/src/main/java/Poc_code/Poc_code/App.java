148b5fd5239c006434bf63cd4ea3599491693212
EI13213 <EI13213@ELW4297.corpindia.local>
2022-11-27 19:58:33 +0530

    code

---
+++ Poc_code/src/main/java/Poc_code/Poc_code/App.java
@@ -0,0 +1,13 @@
+package Poc_code.Poc_code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
