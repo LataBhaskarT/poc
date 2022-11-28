3b879d3dcedd7a9e968287f453aea92dc330980d
EI13213 <EI13213@ELW4297.corpindia.local>
2022-11-28 15:41:18 +0530

    Leave page and script

---
+++ Poc_code/src/test/java/pages/ApplyLeavePage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import utility.Baseclass;
+
+public class ApplyLeavePage extends Baseclass{
+	
+	@FindBy(css="button[title='Apply Leave']") WebElement applyLeavel;
+	@FindBy(css="div.oxd-select-wrapper") WebElement selectLeaveType;
+	@FindBy(css="div.oxd-date-input input") List<WebElement> dates;
+	@FindBy(css="textarea.oxd-textarea") WebElement comments;
+	@FindBy(css="button[type='submit']") WebElement applyBttn;
+	
+	
+	public ApplyLeavePage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickOnApplyLeave() {
+		applyLeavel.click();		
+	}
+	
+	public void fillLeaveDetails() {
+		Select selectLeave = new Select(selectLeaveType);
+		selectLeave.selectByVisibleText("CAN - Bereavement");
+	}
+}
---
+++ Poc_code/src/test/java/scripts/ApplyLeave.java
@@ -0,0 +1,21 @@
+package scripts;
+
+import org.testng.annotations.Test;
+
+import pages.ApplyLeavePage;
+import pages.LoginPage;
+
+public class ApplyLeave extends ApplyLeavePage {
+	
+	@Test
+	public void testApplyLeave() {
+	ApplyLeavePage applyLeave=new ApplyLeavePage();
+	LoginPage loginPage=new LoginPage();
+	loginPage.login();
+	loginPage.verifyUserLoggedIn();
+	applyLeave.clickOnApplyLeave();
+	applyLeave.fillLeaveDetails();
+	}
+	
+
+}
